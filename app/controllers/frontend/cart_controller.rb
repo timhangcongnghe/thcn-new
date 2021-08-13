aaef9d55b46928243068b0ed988eecbf9c74dd8b
Nguyen Ngoc Son <sonnn0811@icloud.com>
2021-08-13 16:01:46 +0700

    udpate

--- app/controllers/frontend/cart_controller.rb
+++ app/controllers/frontend/cart_controller.rb
@@ -6,7 +6,7 @@ module Frontend
     end
     
     def checkout
-      @class = ''
+      @class = 'page-template-default page wp-custom-logo wp-embed-responsive theme-eletra thcn-checkout thcn-page thcn-no-js wpisset wpisset-checkout wpisset-no-sidebar wpisset-sticky-nav wpisset-footer-bottom-active wpisset-responsive-breakpoints wpisset-medium-breakpoint-768 wpisset-desktop-breakpoint-992 wpisset-header-nav-variant2 wpisset-woo-sidebar-left wpisset-yith-wishlist wpisset-yith-compare wpb-js-composer js-comp-ver-6.6.0 vc_responsive'
     end
   end
 end
