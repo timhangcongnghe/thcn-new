31b515afbe40d7411ddcb8b05379d26ffab3ae6d
Nguyen Ngoc Son <sonnn0811@icloud.com>
2021-09-15 09:49:40 +0700

    update

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,6 +1,6 @@
 class Menu < ApplicationRecord
   self.table_name = 'erp_menus_menus'
-  belongs_to :parent, class_name: "Menu", optional: true
+  belongs_to :parent, class_name: 'Menu'
   has_many :children, -> {order(custom_order: :asc)}, class_name: 'Menu', foreign_key: 'parent_id'
   has_and_belongs_to_many :categories, class_name: 'Category'
   
